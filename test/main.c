db21ead181119c877870dc1bae8a667f7ffb4816
Michal Hloušek <mhlous@gmail.com>
2016-07-06 15:39:20 +0200

    Rename testing framework

---
+++ test/main.c
@@ -0,0 +1,13 @@
+/*
+ * @brief: C console testing framework
+ * @author: Michal H.
+ * @email: MHlous@gmail.com
+ *
+ **/
+#define EXIT_SUCCESS 0;
+#define EXIT_FAILURE 1;
+
+int main(int argc, char* argv[])
+{
+	return EXIT_SUCCESS;
+}
