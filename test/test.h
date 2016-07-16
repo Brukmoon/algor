eff7973466654d20aa8f34a2076dce3d479ec5f1
Michal Hloušek <mhlous@gmail.com>
2016-07-16 11:24:46 +0200

    Added more testing macros.

--- test/test.h
+++ test/test.h
@@ -23,8 +23,18 @@
 #define TEST_CASE_RUN(name) TEST_CASE_PREFIX()##name##()
 
 #define CASE_INFO() TEST_LOG("\n-- CASE %s --\n", __FUNCTION__)
-#define SECTION(id) TEST_LOG(": SECTION %s\n", id);
-#define REQUIRE(expr) if(expr) { TEST_PASS(":: TEST PASSED ::\n"); } else { TEST_FAIL("xx TEST FAILED xx\n\t#%d on line %d\n\tin file %s\n", test_count + 1, __LINE__, __FILE__); } test_count++
+#define SECTION(id) TEST_LOG(": SECTION %s\n", id)
+// \brief Standard message REQUIRE parameters
+#define FLF __FILE__, __LINE__, __FUNCTION__
+#define REQUIRE(expr, err_format, ...) if (!(expr)) { TEST_LOG(err_format, __VA_ARGS__); test_count_failed++; } test_count++
+#define REQUIRE_SORTED(arr, size, object_size) REQUIRE(sorted(arr, size, object_size), "%s(%d): error in %s: \"array sorted\" failed", FLF))
+#define REQUIRE_EQ(first, second) REQUIRE((first) == (second), "%s(%d): error in %s: \"%d == %d\" failed", FLF, first, second)
+#define REQUIRE_LESS(less, more) REQUIRE((less) < (more), "%s(%d): error in %s: \"%d < %d\" failed", FLF, less, more)
+#define REQUIRE_MORE(more, less) REQUIRE((more) > (less), "%s(%d): error in %s: \"%d > %d\" failed", FLF, more, less)
+#define REQUIRE_TRUE(expr) REQUIRE(expr, "%s(%d): error in %s: \"expression true\" failed", FLF)
+#define REQUIRE_FALSE(expr) REQUIRE(expr, "%s(%d): error in %s: \"expression false\" failed", FLF)
+
+#define CRASH()
 
 // \brief Target stream for logging.
 #define TEST_OUT_TARGET stderr
@@ -37,7 +47,7 @@
 #define TEST_FAIL(format, ...) test_count_failed++; TEST_LOG(format, __VA_ARGS__)
 
 // selects main implementation file
-#define TEST_CONFIG_MAIN int main(int argc, char** argv) { for(int i = 0; i < argc; ++i) TEST_LOG("%s \n", argv[i]); TEST_RUN_IT(); final_statistics(); return test_count_failed; }
+#define TEST_CONFIG_MAIN int main(void) { TEST_LOG("%s %s\n", __DATE__, __TIME__); TEST_RUN_IT(); final_statistics(); return test_count_failed; }
 // Tests the main runs
 #define TEST_RUN_IT() run_tests()
 // function name prefix to prevent clashes
