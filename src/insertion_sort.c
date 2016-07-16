5df13a38dfa7d7059d485d76f2e6735e52d84500
Michal Hloušek <mhlous@gmail.com>
2016-07-16 23:05:39 +0200

    Add insertion_sort implementation

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,15 +1,24 @@
+#include <stdio.h>
+#include <string.h>
 #include "simple_sorts.h"
 
 // WIP
 void insertion_sort(void const* base, size_t const base_size, size_t const set_size, compare less)
 {
-	for (size_t i = 1; i < set_size - 1; ++i)
+	if (base == NULL || less == NULL || base_size == 0)
+		return;
+	void *temp = malloc(base_size);
+	for (size_t i = 1; i < set_size; ++i)
 	{
-		for (size_t j = i; j >= 1; --j)
+		// temp = arr[i]
+		memcpy(temp, ELEMENT(i, base, base_size), base_size);
+		size_t j = i - 1;
+		while((j <= set_size) && less(temp, ELEMENT(j, base, base_size)))
 		{
-			if (less(ELEMENT(j, base, base_size), ELEMENT(j - 1, base, base_size)))
-				swap(ELEMENT(j, base, base_size), ELEMENT(j - 1, base, base_size), base_size);
-			else break;
+			memcpy(ELEMENT(j + 1, base, base_size), (ELEMENT(j, base, base_size)), base_size);
+			--j;
 		}
+		memcpy(ELEMENT(j + 1, base, base_size), temp, base_size);
 	}
+	free(temp);
 }
