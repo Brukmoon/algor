597469bbf0518aee93ca13594196bd738959f7f8
Michal Hloušek <mhlous@gmail.com>
2016-07-19 13:12:00 +0200

    Finish binary insertion sort.

--- src/array/binary_insertion_sort.c
+++ src/array/binary_insertion_sort.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <string.h>
 #include "binary_search.h"
 #include "simple_sorts.h"
@@ -14,14 +15,14 @@ void binary_insertion_sort(void *const base, size_t const base_size, size_t cons
 	void *temp = malloc(base_size);
 	if (temp == NULL)
 		alloc_err();
-	for (size_t i = 1, k = 0; i < set_size; ++i)
+	for (size_t i = 1; i < set_size; ++i)
 	{
-		// arr[k] is where arr[i] belongs
-		k = approx_binary_search(ELEMENT(i, base, base_size), base, base_size, set_size, compare);
+		// search for arr[i]'s place in arr[0]:arr[i]
+		int k = approx_binary_search(ELEMENT(i, base, base_size), base, base_size, i+1, compare);
 		// temp = arr[i] -- backup
 		memcpy(temp, ELEMENT(i, base, base_size), base_size);
 		// shift values right from arr[k] to arr[i]
-		shift_right(ELEMENT(k, base, base_size), base_size, i - k);
+		shift_right(ELEMENT(k, base, base_size), base_size, i-k);
 		// copy backup value where it belongs
 		memcpy(ELEMENT(k, base, base_size), temp, base_size);
 	}
@@ -31,29 +32,35 @@ void binary_insertion_sort(void *const base, size_t const base_size, size_t cons
 static size_t approx_binary_search(void *value, void *const base, size_t const base_size, size_t const set_size,
 	int(*compare)(const void *const first, const void *const second))
 {
-	size_t L = 0, H = set_size - 1, k = 0, hop = 1;
+	// low:high, k in between
+	size_t L = 0, H = set_size - 1, k = 0;
+	
+	// reduce the interval searched
+	size_t hop = 1;
 	do
 	{
 		k = H - hop;
 		hop *= 2;
+		// if value < arr[k]
 		if (compare(value, ELEMENT(k, base, base_size)) == LESS)
 			H = k;
 		else
 			L = k + 1;
-	} while (H - hop >= L);
-	int index = 0;
+	} while (L + hop <= H);
+	// if L == H before the actual binary sort is entered, we found our value
 	while (L != H)
 	{
 		k = (L + H) / 2;
 		if (compare(value, ELEMENT(k, base, base_size)) == LESS)
 		{
+			if (k - 1 > H) // integer overflow
+				break;
 			H = k - 1;
-			index = k;
 		}
 		else
 			L = k + 1;
 	}
-	return index;
+	return L;
 }
 
 static void shift_right(void *const base, size_t const base_size, size_t const shift_count)
